04c9915a4216d8c0d497a92b3c28057b8ffc687b
Allen Wright <allenwrightkc@gmail.com>
2020-10-28 20:49:11 -0700

    working code for one page, adds gitignore for tests

--- content_script.js
+++ content_script.js
@@ -1,21 +1,32 @@
-let results = [];
-
-function parsePage() {
+function parsePage(results) {
+  let button;
+    // gets all links
   let items = document.querySelectorAll('a.ems-sdk-product-tile-link');
   for (let i = 0; i < items.length; i++) {
     let item = JSON.parse(items[i].dataset.telemetryMeta);
-    results.push({
+    let result = {
       name: item.name,
-      price: item.price,
       link: `https://store.playstation.com/product/${item.id}`
-    });
-  }
-  let button = document.querySelector('.psw-icon--chevron-right').parentElement;
-  // recursive call to run parsePage again if there's another page
-  if (!button.classList.contains('psw-is-disabled')) {
-    button.click();
-    parsePage();
-  }
+    }
+    let priceArray = items[i].children[1].innerText.split('\n');
+    // different logic based on if there's a PS+ discount or not
+    if (priceArray.length === 3) {
+      result.psPercentage = 'N/A';
+      result.psPrice = 'N/A';
+      result.salePercentage = priceArray[0];
+      result.salePrice = priceArray[1];
+      result.originalPrice = priceArray[2];
+    } else if (priceArray.length === 4) {
+      let psDiscount = parseInt(priceArray[1]) - priceArray[0].match(/[0-9]{1,2}/)[0];
+      result.psPercentage = `${psDiscount}%`;
+      result.salePercentage = priceArray[1];
+      result.salePrice = priceArray[2];
+      result.originalPrice = priceArray[3];
+      result.psPrice = '$' + ((parseFloat(priceArray[3].substring(1)) * (1 - (Math.abs(psDiscount)/100))).toFixed(2) - 0.01);
+    }
+      results.push(result);
+    }
+  return results;
 }
-parsePage();
-results;
+
+parsePage([]);
--- popup.js
+++ popup.js
@@ -1,7 +1,14 @@
 let parseButton = document.getElementById('parseButton');
+let textarea = document.getElementById('markdown-preview');
 
 let callback = function(results) {
   console.log(results);
+  let games = results[0];
+  games.sort((a, b) => a.name.localeCompare(b.name));
+  textarea.value = `| Game | Price | % Off | PS+ Price | PS+ % Off |\n| --- | --- | --- | --- | --- |`;
+  for (let i = 0; i < games.length; i++) {
+    textarea.value += `\n[${games[i].name}](${games[i].link}) | ${games[i].salePrice} | ${games[i].salePercentage} | ${games[i].psPrice} | ${games[i].psPercentage}`;
+  }
 }
 
 parseButton.onclick = function() {
