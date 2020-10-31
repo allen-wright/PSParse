6c1ab56bb8c6ca8c2d76801b8c5ff83646d0ff26
Allen Wright <allenwrightkc@gmail.com>
2020-10-30 17:58:52 -0700

    adds new icons, modifies manifest

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.local.set({games: [], markdown: '| Game | Price | % Off | PS+ Price | PS+ % Off |\n| --- | --- | --- | --- | --- |'}, () => console.log('games array initialized.'));
-});
+  chrome.storage.local.set({games: [], markdown: '| Game | Price | % Off | PS+ Price | PS+ % Off |\n| --- | --- | --- | --- | --- |'});
 
-chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
